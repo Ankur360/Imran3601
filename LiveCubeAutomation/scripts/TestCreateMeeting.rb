0b3e74c4c5794759b9403d32737f7fe72feac452
unknown <ankurp@360logica.com>
2014-09-30 19:16:18 +0530

    new script

--- LiveCubeAutomation/scripts/TestCreateMeeting.rb
+++ LiveCubeAutomation/scripts/TestCreateMeeting.rb
@@ -56,7 +56,7 @@ describe "Scenario test create meeting" do
       message= dashboard.verifyMeetingCreated()
       expect(message).to include("Meeting was successfully created.")
 
-      puts "Test create Meeting method completed successfuly"
+      puts "Test create Meeting method completed succesfully"
 
     rescue
       CreateLog.new().Log("open application url")
