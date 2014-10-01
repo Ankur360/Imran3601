c5ef1e76bf45de49fcf1122a3e60529e0f7ba314
nischalk <nischalk@360logica.com>
2014-10-01 19:39:00 +0530

    Testing

---
+++ LiveCubeAutomation/test_suite - Copy.rb
@@ -0,0 +1,44 @@
+path = File.expand_path(File.dirname(__FILE__))
+#Dir[path+"/scripts/*.rb"].each {|file| require file }
+require path+"/scripts/TestCreateMeeting.rb"
+require path+"/scripts/TestCreateSpeaker.rb"
+require path+"/scripts/TestCreateSession.rb"
+require path+"/scripts/TestCreateTeam.rb"
+require path+"/scripts/TestCreatePrize.rb"
+require path+"/scripts/TestCreateSurvey.rb"
+require path+"/scripts/TestProjectView.rb"
+require path+"/scripts/TestCreateNewIceBreaker.rb"
+require path+"/scripts/TestLoginLiveCubeFirstTimeEmail.rb"
+require path+"/scripts/TestLoginLiveCubeFirstTimeTwitter.rb"
+require path+"/scripts/TestLoginLiveCubeEmail.rb"
+require path+"/scripts/TestLoginLiveCubeTwitter.rb"
+require path+"/scripts/TestForgotPasswordTwitter.rb"
+require path+"/scripts/TestAddJoinSession.rb"
+require path+"/scripts/TestPostMessage.rb"
+require path+"/scripts/TestRePostFavorite.rb"
+require path+"/scripts/TestRateSession.rb"
+require path+"/scripts/TestFlagAndReply.rb"
+require path+"/scripts/TestRecentPopularSorting.rb"
+require path+"/scripts/TestAttendeesAndSpeaker.rb"
+require path+"/scripts/TestVerifyInformation.rb"
+require path+"/scripts/TestVerifyUserActivity.rb"
+require path+"/scripts/TestUpdateUserProfile.rb"
+require path+"/scripts/TestRemovePost.rb"
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
